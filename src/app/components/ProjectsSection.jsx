f43b6008480deae7a570a6a114c56b51df4764d6
Lucas Fernandez <lucasfernandezc@outlook.com>
2024-04-06 15:23:55 -0300

    static images

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -7,28 +7,28 @@ const projectsData = [
         id: 1,
         title: "Landing page",
         description: "Landing page para un hotel de mascotas",
-        image: "/images/landing_page.png",
+        image: "static/images/landing_page.png",
         tag: ["All", "Web"]
     },
     {
         id: 2,
         title: "Bookstore",
         description: "Marketplace de libros",
-        image: "/images/Bookstore.png",
+        image: "/static/images/Bookstore.png",
         tag: ["All", "Web"]
     },
     {
         id: 3,
         title: "Dashboard Cripto",
         description: "Consumo de api para crear dashboard",
-        image: "/images/dashboard_cripto.png",
+        image: "/static/images/dashboard_cripto.png",
         tag: ["All", "Web"]
     },
     {
         id: 4,
         title: "Crud",
         description: "Crud con operaciones basicas",
-        image: "/images/Crud.png",
+        image: "/static/images/Crud.png",
         tag: ["All", "Web"]
     }
 ]
@@ -36,7 +36,7 @@ const projectsData = [
 const ProjectsSection = () => {
     return (
         <>
-            <h2>Mis proyectos</h2>
+            <h2 className='text-center text-4xl font-bold text-white mt-4 mb-4 '>Mis proyectos</h2>
             <div className='grid md:grid-cols-3 gap-8 md:gap-12'>
                 {projectsData.map((project) =>
                     <ProjectsCard
